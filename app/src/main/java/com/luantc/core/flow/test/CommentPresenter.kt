970f36691cdf99b4fa087a58dd0cb93891c9bd17
Luan Truong <luan.truong@persol.co.jp>
2020-06-18 10:20:15 +0700

    coroutines ext

--- app/src/main/java/com/luantc/core/flow/test/CommentPresenter.kt
+++ app/src/main/java/com/luantc/core/flow/test/CommentPresenter.kt
@@ -1,14 +1,27 @@
 package com.luantc.core.flow.test
 
+import com.bennyhuo.kotlin.coroutines.android.mainscope.scope.BasicScoped
+import com.bennyhuo.kotlin.coroutines.android.mainscope.scope.withMainScope
 import com.luantc.core.api.GeneralErrorHandler
 import com.luantc.core.manager.ApiManager
 import com.luantc.core.mvp.BaseMvpPresenterImpl
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import rx.functions.Action1
 
 class CommentPresenter : BaseMvpPresenterImpl<CommentContract.View>(),
-    CommentContract.Presenter {
+    CommentContract.Presenter, BasicScoped {
 
     override fun loadComment() {
+        withMainScope {
+            launch {
+
+            }
+            async(Dispatchers.IO) {
+
+            }
+        }
         mView?.showLoading("Helllooooooo....")
         ApiManager.loadComment()
             .doOnError { mView?.showError(it.toString()) }
